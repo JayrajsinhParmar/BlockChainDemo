98c58eaf877a117642762014ded3f077028bbb69
Jayrajsinh <jayrajsinh@ifourtechnolab.com>
2022-04-01 12:43:49 +0530

    created a db

--- contracts/todoList.sol
+++ contracts/todoList.sol
@@ -3,4 +3,22 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
     uint256 public todoCount = 0;
+
+    struct Task {
+        uint id;
+        string taskName;
+        bool isCompleted;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public
+    {
+        createNewTask("Inital Task");
+    }
+
+    function createNewTask(string memory taskName) public {
+        todoCount++;
+        tasks[todoCount] = Task(todoCount, taskName, false);
+    }
 }
