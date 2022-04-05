1b4c47cd4688da5a1eca272be71136e56c3d0f6e
Jayrajsinh <jayrajsinh@ifourtechnolab.com>
2022-04-05 10:19:00 +0530

    crud

--- contracts/todoList.sol
+++ contracts/todoList.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+pragma experimental ABIEncoderV2;
 
 contract TodoList {
     uint256 public todoCount = 0;
@@ -12,13 +13,38 @@ contract TodoList {
 
     mapping(uint => Task) public tasks;
 
-    constructor() public
-    {
+    constructor() public {
         createNewTask("Inital Task");
     }
 
     function createNewTask(string memory taskName) public {
         todoCount++;
         tasks[todoCount] = Task(todoCount, taskName, false);
+    }
+
+    function udpateTask(
+        uint _id,
+        string memory _taskname,
+        bool isCompleted
+    ) public {
+        uint todoIndex = findById(_id);
+        tasks[todoIndex].taskName = _taskname;
+        tasks[todoIndex].isCompleted = isCompleted;
+    }
+
+    function findById(uint _id) internal view returns (uint) {
+        for (uint i = 0; i <= todoCount; i++) {
+            if (tasks[i].id == _id) {
+                return i;
+            }
+        }
+    }
+
+    function getAll() public view returns (Task[] memory) {
+        Task[] memory ret = new Task[](todoCount);
+        for (uint i = 0; i < todoCount; i++) {
+            ret[i] = tasks[i];
+        }
+        return ret;
     }
 }
